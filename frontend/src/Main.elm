97aff281516c5df9d7f8703157948965c4e9924d
Jinjun Liang <kumkee@users.noreply.github.com>
2023-03-03 07:13:39 +1300

    added UserName and PasswordChanged

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (main)
 import Browser
 import Html exposing (Html, button, div, input, table, td, text, tr)
 import Html.Attributes exposing (placeholder, type_, value)
+import Html.Events exposing (onInput)
 
 
 main : Program () Model Msg
@@ -23,20 +24,24 @@ init () =
 view : Model -> Html Msg
 view model =
     case model.status of
-        Logout form->
+        Logout form ->
             table []
                 [ tr []
                     [ td [] [ text "Username: " ]
                     , td []
-                        [ input [ placeholder "username", value form.username ]
-                            []
+                        [ viewInput "text"
+                            "username"
+                            form.username
+                            UserNameChanged
                         ]
                     ]
                 , tr []
                     [ td [] [ text "Password: " ]
                     , td []
-                        [ input [ type_ "password", placeholder "password" ]
-                            []
+                        [ viewInput "password"
+                            "password"
+                            form.password
+                            PasswordChanged
                         ]
                     ]
                 , tr [] [ td [] [], button [] [ text "Login" ] ]
@@ -46,15 +51,30 @@ view model =
             div [] [ text token ]
 
 
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+    input [ type_ t, placeholder p, value v, onInput toMsg ] []
+
+
 type Msg
     = GotToken String
+    | UserNameChanged String
+    | PasswordChanged String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
-        GotToken token ->
+    case (msg, model.status) of
+        (GotToken token, _) ->
             ( { model | token = token, status = Login token }, Cmd.none )
+        (UserNameChanged username, Logout form) ->
+            ( { model | status = Logout {form | username = username}}, Cmd.none)
+        (PasswordChanged password, Logout form) ->
+            ( { model | status = Logout {form | password = password}}, Cmd.none)
+        (_, Login _) ->
+            (model, Cmd.none)
+
+                    
 
 
 type alias Model =
@@ -71,7 +91,7 @@ type alias User =
     }
 
 
-type alias Form = 
+type alias Form =
     { username : String
     , password : String
     }
