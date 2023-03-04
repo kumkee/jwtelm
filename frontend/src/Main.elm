724de77ee548e4da3ece41f5257130e638ca8848
Jinjun Liang <kumkee@users.noreply.github.com>
2023-03-04 22:03:54 +1300

    model.status

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -28,12 +28,19 @@ main =
 
 init : () -> ( Model, Cmd Msg )
 init () =
-    ( SignedOut <| Form "" "", Cmd.none )
+    ( initialModel, Cmd.none )
+
+
+initialModel : Model
+initialModel =
+    { token = Token "" ""
+    , status = SignedOut <| Form "" ""
+    }
 
 
 view : Model -> Html Msg
 view model =
-    case model of
+    case model.status of
         SignedOut form ->
             table []
                 [ tr []
@@ -57,10 +64,10 @@ view model =
                 , tr [] [ td [] [], button [ onClick Login ] [ text "Login" ] ]
                 ]
 
-        SignedIn token Nothing ->
-            pre [] [ text token ]
+        SignedIn Nothing ->
+            pre [] [ text model.token.tokenString ]
 
-        SignedIn _ (Just user) ->
+        SignedIn (Just user) ->
             div []
                 [ text "User: "
                 , text user.username
@@ -73,7 +80,10 @@ view model =
                 ]
 
         Loading target ->
-            text <| "Loading" ++ target ++ "..."
+            text <| "Loading " ++ target ++ "..."
+
+        Error err ->
+            text <| "Error: " ++ err
 
 
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
@@ -85,33 +95,41 @@ type Msg
     = Login
     | UserNameChanged String
     | PasswordChanged String
-    | GotToken (Result Http.Error String)
+    | GotToken (Result Http.Error Token)
     | GotUser (Result Http.Error User)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case ( msg, model ) of
+    case ( msg, model.status ) of
         ( UserNameChanged username, SignedOut form ) ->
-            ( SignedOut { form | username = username }, Cmd.none )
+            ( { model | status = SignedOut { form | username = username } }
+            , Cmd.none
+            )
 
         ( PasswordChanged password, SignedOut form ) ->
-            ( SignedOut { form | password = password }, Cmd.none )
+            ( { model | status = SignedOut { form | password = password } }
+            , Cmd.none
+            )
 
         ( Login, SignedOut form ) ->
-            ( Loading "access token", loginCmd form )
+            ( { model | status = Loading "access token" }, loginCmd form )
 
         ( GotToken (Ok token), _ ) ->
-            ( SignedIn token Nothing, getUserCmd token )
+            ( { model | status = Loading "user" }, getUserCmd token )
 
-        ( GotUser (Ok user), SignedIn token Nothing ) ->
-            ( SignedIn token <| Just user, Cmd.none)
+        ( GotUser (Ok user), _ ) ->
+            ( { model | status = SignedIn <| Just user }, Cmd.none )
 
         ( GotToken (Err error), _ ) ->
-            ( SignedIn ("GotToken: " ++ Debug.toString error) Nothing, Cmd.none )
+            ( { model | status = Error ("GotToken: " ++ Debug.toString error) }
+            , Cmd.none
+            )
 
         ( GotUser (Err error), _ ) ->
-            ( SignedIn ("GotUser: " ++ Debug.toString error) Nothing, Cmd.none )
+            ( { model | status = Error ("GotUser: " ++ Debug.toString error) }
+            , Cmd.none
+            )
 
         ( _, _ ) ->
             ( model, Cmd.none )
@@ -129,8 +147,15 @@ loginCmd form =
     Http.post
         { url = baseUrl ++ "token"
         , body = body
-        , expect = Http.expectString GotToken
+        , expect = Http.expectJson GotToken tokenDecoder
         }
+
+
+tokenDecoder : Decoder Token
+tokenDecoder =
+    succeed Token
+        |> required "access_token" string
+        |> required "token_type" string
 
 
 userDecoder : Decoder User
@@ -142,14 +167,15 @@ userDecoder =
         |> required "disabled" bool
 
 
-getUserCmd : String -> Cmd Msg
+getUserCmd : Token -> Cmd Msg
 getUserCmd token =
     Http.request
         { method = "GET"
         , headers =
             [ Http.header "accept: application/json" <|
-                "Authorization: Bearer "
-                    ++ token
+                Format.value token.tokenString <|
+                    Format.value token.tokenType <|
+                        "Authorization: {{ }} {{ }}"
             ]
         , url = baseUrl ++ "users/me"
         , body = Http.emptyBody
@@ -159,10 +185,23 @@ getUserCmd token =
         }
 
 
-type Model
+type alias Model =
+    { token : Token
+    , status : Status
+    }
+
+
+type alias Token =
+    { tokenString : String
+    , tokenType : String
+    }
+
+
+type Status
     = SignedOut Form
-    | SignedIn String (Maybe User)
+    | SignedIn (Maybe User)
     | Loading String
+    | Error String
 
 
 type alias User =
